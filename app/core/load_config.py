0f7dbe11c5bf5468cec6f257a9b3370eb5323f51
Oleksandr Karadiaur <alex.karadyaur@outlook.com>
2024-10-31 23:19:43 +0100

    𝌊 core file for config loading

---
+++ app/core/load_config.py
@@ -0,0 +1,12 @@
+import yaml
+from app.models.config_model import ConfigModel
+
+def load_config() -> ConfigModel:
+    with open("config/config.yaml", "r") as file:
+        config_data = yaml.safe_load(file)
+    return ConfigModel(
+        generated_files_path=config_data["paths"]["output_path"],
+        model_name=config_data["chat_gpt"]["model"],
+        max_tokens=config_data["chat_gpt"]["max_tokens"],
+        temperature=config_data["chat_gpt"]["temperature"]
+    )
